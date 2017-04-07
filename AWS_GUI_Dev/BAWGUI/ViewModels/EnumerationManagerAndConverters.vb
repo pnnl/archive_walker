e1545a4d52f7a7be47e29a289feae0ac6b9f5a25
Wang, Heng <heng.wang@pnnl.gov>
2017-04-07 15:56:54 -0700

    final version for the demo

--- AWS_GUI_Dev/BAWGUI/ViewModels/EnumerationManagerAndConverters.vb
+++ AWS_GUI_Dev/BAWGUI/ViewModels/EnumerationManagerAndConverters.vb
@@ -182,7 +182,7 @@ Public Class EmptyHeaderConverter
     End Function
 
     Public Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
-        Throw New NotImplementedException()
+        Return DependencyProperty.UnsetValue
     End Function
 End Class
 
@@ -231,3 +231,19 @@ Public Class CurrentStepNameConverter
         Return DependencyProperty.UnsetValue
     End Function
 End Class
+
+'Public Class VisibleIfNonZeroConverter
+'    Implements IValueConverter
+
+'    Public Function Convert(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object Implements IValueConverter.Convert
+'        If value Is Nothing Then
+'            Return Visibility.Hidden
+'        Else
+'            Return Visibility.Visible
+'        End If
+'    End Function
+
+'    Public Function ConvertBack(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
+'        Throw New NotImplementedException()
+'    End Function
+'End Class
