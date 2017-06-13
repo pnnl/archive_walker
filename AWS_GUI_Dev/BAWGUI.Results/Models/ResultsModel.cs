65427554f988e21d50b20fb1713a0d55cf8f6f43
fitz303 <erin.fitzhenry@pnnl.gov>
2017-06-13 11:17:34 -0700

    Adding results views.

---
+++ AWS_GUI_Dev/BAWGUI.Results/Models/ResultsModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAWGUI.Results.Models
+{
+    class ResultsModel
+    {
+        internal void LoadResults(string filename)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AWS_GUI_Dev/BAWGUI.Results/ViewModels/ResultsViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BAWGUI.Results.Models;
+
+namespace BAWGUI.Results.ViewModels
+{
+    class ResultsViewModel
+    {
+        private ForcedOscillationResultsViewModel _forcedOscillationResultsViewModel = new ForcedOscillationResultsViewModel();
+        private OutOfRangeResultsViewModel _outOfRangeResultsViewModel = new OutOfRangeResultsViewModel();
+        private RingdownResultsViewModel _ringdownResultsViewModel = new RingdownResultsViewModel();
+        private WindRampResultsViewModel _windRampResultsViewModel = new WindRampResultsViewModel();
+        private ResultsModel _resultsModel = new ResultsModel();
+
+        public ResultsViewModel()
+        {
+
+        }
+
+        public void LoadResults(string filename)
+        {
+            this._resultsModel.LoadResults(filename);
+        }
+    }
+}
