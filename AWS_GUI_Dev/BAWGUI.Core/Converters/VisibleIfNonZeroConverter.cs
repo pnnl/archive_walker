88e4db39bfb58d8560368db875bac5e991bea6ed
Wang <heng.wang@pnnl.gov>
2018-07-31 13:31:47 -0700

    moved filterlistbox and a converter to core.

---
+++ AWS_GUI_Dev/BAWGUI.Core/Converters/VisibleIfNonZeroConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace BAWGUI.Core.Converters
+{
+    public class VisibleIfNonZeroConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var val = double.Parse(value.ToString());
+            if (val == 0 ^ (string)parameter == "HideIfNonZero")
+                return Visibility.Hidden;
+            else
+                return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
