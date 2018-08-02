bb45158f75f27fc81d022060bb0f6ee5a9206092
Wang <heng.wang@pnnl.gov>
2018-08-02 13:18:02 -0700

    combined Enumtostringconverters in the core.

--- AWS_GUI_Dev/BAWGUI.Core/Converters/EnumToStringConverters.cs
+++ AWS_GUI_Dev/BAWGUI.Core/Converters/EnumToStringConverters.cs
@@ -48,4 +48,34 @@ namespace BAWGUI.Core.Converters
             }
         }
     }
+    public class EnumToStringConverter11 : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch (value)
+            {
+                case RetroactiveContinuityStatusType.OFF:
+                    return "OFF";
+                case RetroactiveContinuityStatusType.ON:
+                    return "ON";
+                default:
+                    throw new Exception("Retroactive Continuity Status Type not valid!");
+            }
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch (value)
+            {
+                case "OFF":
+                    return RetroactiveContinuityStatusType.OFF;
+
+                case "ON":
+                    return RetroactiveContinuityStatusType.ON;
+
+                default:
+                    throw new Exception("Enum type not valid!");
+            }
+        }
+    }
 }
