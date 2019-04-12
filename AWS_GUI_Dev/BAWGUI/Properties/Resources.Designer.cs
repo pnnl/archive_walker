b8cd2d25180ffd04738ff3a9eb4ce466132577e4
Wang, Heng <heng.wang@pnnl.gov>
2019-04-12 11:56:22 -0700

    changed executable name, added icon on the bottom of project tree.

--- AWS_GUI_Dev/BAWGUI/Properties/Resources.Designer.cs
+++ AWS_GUI_Dev/BAWGUI/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace BAWGUI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Archive_Walker_Gray_Final {
+            get {
+                object obj = ResourceManager.GetObject("Archive_Walker_Gray_Final", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap bigMarkerGreen {
             get {
                 object obj = ResourceManager.GetObject("bigMarkerGreen", resourceCulture);
