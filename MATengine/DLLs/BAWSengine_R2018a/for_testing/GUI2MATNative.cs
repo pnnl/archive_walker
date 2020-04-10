822349774e93c2660f69be4bb90387da83e3990e
Follum <james.follum@pnnl.gov>
2020-04-10 08:48:33 -0700

    Updated DLL for mode meter update.

--- MATengine/DLLs/BAWSengine_R2018a/for_testing/GUI2MATNative.cs
+++ MATengine/DLLs/BAWSengine_R2018a/for_testing/GUI2MATNative.cs
@@ -1,6 +1,6 @@
 /*
 * MATLAB Compiler: 6.6 (R2018a)
-* Date: Thu Mar 19 15:39:20 2020
+* Date: Fri Apr 10 08:40:46 2020
 * Arguments:
 * "-B""macro_default""-W""dotnet:BAWSengine,GUI2MAT,4.0,private""-T""link:lib""-d""C:\User
 * s\foll154\OneDrive -
